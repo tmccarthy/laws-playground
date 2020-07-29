000824527c4606b41520cb5987feb8ad296b1bf3
Timothy McCarthy <ebh042@gmail.com>
2020-07-29 15:55:22 +1000

    Remove code for beginning of presentation

--- examples/src/main/scala/au/id/tmm/lawsplayground/semigroups/presentation/Laws.scala
+++ examples/src/main/scala/au/id/tmm/lawsplayground/semigroups/presentation/Laws.scala
@@ -1,62 +1,11 @@
 package au.id.tmm.lawsplayground.semigroups.presentation
 
-import au.id.tmm.lawsplayground.semigroups.syntax._
-import au.id.tmm.lawsplayground.semigroups.{Instance, Law}
-import cats.kernel.laws.IsEq
+//import au.id.tmm.lawsplayground.semigroups.syntax._
+//import au.id.tmm.lawsplayground.semigroups.{Instance, Law}
+//import cats.kernel.laws.IsEq
 
 object Laws {
 
-  val binaryOperation: Law = new Law.With2Params(name = "binary operation") {
-    override def test[A: Instance](a1: A, a2: A): IsEq[A] =
-      (a1 + a2) alwaysEquals (a1 + a2)
-  }
 
-  val associativity: Law = new Law.With3Params(name = "associativity") {
-    override def test[A: Instance](a1: A, a2: A, a3: A): IsEq[A] =
-      ((a1 + a2) + a3) alwaysEquals (a1 + (a2 + a3))
-  }
-
-  val identity: Law = new Law.With1Param(name = "identity") {
-    override def test[A: Instance](a: A): IsEq[A] =
-      (a + zero) alwaysEquals a
-  }
-
-  val idempotence: Law = new Law.With1Param(name = "idempotence") {
-    override def test[A: Instance](a: A): IsEq[A] =
-      (a + a) alwaysEquals a
-  }
-
-  val commutativity: Law = new Law.With2Params(name = "commutativity") {
-    override def test[A: Instance](a1: A, a2: A): IsEq[A] =
-      (a1 + a2) alwaysEquals (a2 + a1)
-  }
-
-  val inverse: Law = new Law.With1Param(name = "inverse") {
-    override def test[A: Instance](a: A): IsEq[A] =
-      ((-a) + a) alwaysEquals zero
-  }
-
-  val associativeMultiplication: Law = new Law.With3Params(name = "associative multiplication") {
-    override def test[A: Instance](a1: A, a2: A, a3: A): IsEq[A] =
-      ((a1 * a2) * a3) alwaysEquals (a1 * (a2 * a3))
-  }
-
-  val multiplicativeIdentity: Law = new Law.With1Param(name = "multiplicative identity") {
-    override def test[A: Instance](a: A): IsEq[A] =
-      (a * one) alwaysEquals a
-  }
-
-  val distributive: Law = new Law.With3Params(name = "distributive") {
-    override def test[A: Instance](a1: A, a2: A, a3: A): IsEq[A] =
-      (a1 * (a2 + a3)) alwaysEquals ((a1 * a2) + (a1 * a3))
-  }
-
-  val multiplicativeInverse: Law = new Law.With1Param(name = "multiplicative inverse") {
-    override def test[A: Instance](a: A): IsEq[A] =
-      a.`⁻¹` match {
-        case Some(inverse) => if (a === zero) fail else (a * inverse) alwaysEquals one
-        case None => if (a === zero) pass else fail
-      }
-  }
 
 }
--- examples/src/main/scala/au/id/tmm/lawsplayground/semigroups/presentation/Presentation.scala
+++ examples/src/main/scala/au/id/tmm/lawsplayground/semigroups/presentation/Presentation.scala
@@ -12,35 +12,18 @@ object Presentation extends LawsPlayground {
       Instance[Int](
         name = "Int under addition",
         binaryOp = (i1: Int, i2: Int) => i1 + i2,
-        identity = 0,
-        inverse = (i: Int) => -i,
-        multiplicativeOp = (i1: Int, i2: Int) => i1 * i2,
-        multiplicativeIdentity = 1,
-        multiplicativeInverse = (i: Int) => i match {
-          case 0 => None
-          case i => Some(1 / i)
-        }
       ),
       Instance[Int](
         name = "Int under maximum",
         binaryOp = (i1: Int, i2: Int) => i1 max i2,
-        identity = Int.MinValue,
       ),
       Instance[Rational](
         name = "Rational under addition",
         binaryOp = (r1: Rational, r2: Rational) => r1 + r2,
-        identity = 0d,
-        inverse = (r: Rational) => r * -1,
-        multiplicativeOp = (r1: Rational, r2: Rational) => r1 * r2,
-        multiplicativeIdentity = 1d,
-        multiplicativeInverse = (r: Rational) => r match {
-          case Rational.zero => None
-          case r => Some(r.inverse)
-        }
       ),
       Instance[String](
+        name = "String under concatenation",
         binaryOp = (s1: String, s2: String) => s1 + s2,
-        identity = "",
       ),
       Instance[String](
         name = "String under interleave",
@@ -48,123 +31,21 @@ object Presentation extends LawsPlayground {
       ),
       Instance[List[String]](
         binaryOp = (l1: List[String], l2: List[String]) => l1 ++ l2,
-        identity = List.empty,
       ),
       Instance[NonEmptyList[String]](
         binaryOp = (nel1: NonEmptyList[String], nel2: NonEmptyList[String]) => nel1.concatNel(nel2),
       ),
       Instance[Set[String]](
         binaryOp = (l1: Set[String], l2: Set[String]) => l1 ++ l2,
-        identity = Set.empty[String],
       ),
       Instance[SquareMatrix](
         binaryOp = (m1: SquareMatrix, m2: SquareMatrix) => m1 + m2,
-        identity = SquareMatrix.zero,
-        inverse = (m: SquareMatrix) => m * -1,
-        multiplicativeOp = (m1: SquareMatrix, m2: SquareMatrix) => m1 * m2,
-        multiplicativeIdentity = SquareMatrix.I,
       ),
     )
 
   override def typeClasses: List[TypeClass] = {
-    val magma = TypeClass(
-      name = "Magma",
-      parents = Set.empty,
-      laws = Set(
-        Laws.binaryOperation,
-      )
-    )
-
-    val semigroup = TypeClass(
-      name = "Semigroup",
-      parents = Set(magma),
-      laws = Set(
-        Laws.associativity,
-      ),
-    )
-
-    val monoid = TypeClass(
-      name = "Monoid",
-      parents = Set(semigroup),
-      laws = Set(
-        Laws.identity,
-      ),
-    )
-
-    val band = TypeClass(
-      name = "Band",
-      parents = Set(semigroup),
-      laws = Set(
-        Laws.idempotence,
-      ),
-    )
-
-    val semilattice = TypeClass(
-      name = "Semilattice",
-      parents = Set(band),
-      laws = Set(
-        Laws.commutativity,
-      ),
-    )
-
-    val commutativeMonoid = TypeClass(
-      name = "Commutative Monoid",
-      parents = Set(monoid),
-      laws = Set(
-        Laws.commutativity,
-      ),
-    )
-
-    val boundedSemilattice = TypeClass(
-      name = "Bounded Semilattice",
-      parents = Set(semilattice, commutativeMonoid),
-      laws = Set.empty,
-    )
-
-    val group = TypeClass(
-      name = "Group",
-      parents = Set(monoid),
-      laws = Set(
-        Laws.inverse,
-      ),
-    )
-
-    val commutativeGroup = TypeClass(
-      name = "Commutative Group",
-      parents = Set(group, commutativeMonoid),
-      laws = Set.empty,
-    )
-
-    val ring = TypeClass(
-      name = "Ring",
-      parents = Set(commutativeGroup),
-      laws = Set(
-        Laws.associativeMultiplication,
-        Laws.multiplicativeIdentity,
-        Laws.distributive,
-      ),
-    )
-
-    val field = TypeClass(
-      name = "Field",
-      parents = Set(ring),
-      laws = Set(
-        Laws.multiplicativeInverse,
-      ),
-    )
 
     List(
-      magma,
-      semigroup,
-      monoid,
-      band,
-      semilattice,
-      commutativeMonoid,
-      boundedSemilattice,
-      group,
-      commutativeGroup,
-      ring,
-      field,
     )
   }
 }
